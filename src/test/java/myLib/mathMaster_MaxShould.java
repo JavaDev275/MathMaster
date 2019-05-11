e0c755d7ebfb25423fe57a7ee76f5c19aed152f0
JavaDev275 <michael.rossiytsev@azimuth-grp.com>
2019-05-10 23:26:43 -0700

    Update-3: Theories

--- src/test/java/myLib/mathMaster_MaxShould.java
+++ src/test/java/myLib/mathMaster_MaxShould.java
@@ -2,7 +2,10 @@ package myLib;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.theories.*;
+import org.junit.runner.RunWith;
 
+@RunWith(Theories.class)
 public class mathMaster_MaxShould
 {
     private MathMaster mathMaster;
@@ -13,9 +16,13 @@ public class mathMaster_MaxShould
     }
 
     @Test
-    public void return0GivenValuesOf0()
+    public void returnValueWhenValueOfOperandsSame(int value1, int value2, int expectedResult)
     {
-        int result = mathMaster.max(0,0);
-        Assert.assertEquals(0, result);
+        int result = mathMaster.max(value1,value2);
+        Assert.assertEquals(expectedResult, result);
     }
+
+    @DataPoints public static int[] firstTest = {89,89,89};
+    @DataPoints public static int[] secondTest = {-40789,-40789,-40789};
+    @DataPoints public static int[] thirdTest = {275,275,275};
 }
