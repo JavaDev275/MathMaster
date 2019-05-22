475fe10b740247a5f8ee17e184d0e69531f9c7ea
JavaDev275 <michael.rossiytsev@azimuth-grp.com>
2019-05-21 18:09:50 -0700

    Update-2

--- src/main/java/myLib/MathMaster.java
+++ src/main/java/myLib/MathMaster.java
@@ -1,11 +1,9 @@
 package myLib;
 
-public class MathMaster 
-{
-    public int max(int x, int y)
-    {
-        if(x == 0 && y == 0)
-        {
+public class MathMaster {
+    
+    public int max(int x, int y) {
+        if(x == 0 && y == 0) {
             return 0;
         }
         throw new UnsupportedOperationException();
--- src/test/java/myLib/mathMaster_MaxShould.java
+++ src/test/java/myLib/mathMaster_MaxShould.java
@@ -3,18 +3,15 @@ package myLib;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class mathMaster_MaxShould
-{
+public class mathMaster_MaxShould {
     private MathMaster mathMaster;
 
-    public mathMaster_MaxShould()
-    {
+    public mathMaster_MaxShould() {
         mathMaster = new MathMaster();
     }
 
     @Test
-    public void return0GivenValuesOf0()
-    {
+    public void return0GivenValuesOf0() {
         int result = mathMaster.max(0,0);
         Assert.assertEquals(0, result);
     }
